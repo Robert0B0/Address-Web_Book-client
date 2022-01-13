939c2e49eac8a00d6d44228aad6a60a61fa59fa7
Robert0B0 <70260478+Robert0B0@users.noreply.github.com>
2022-01-13 08:57:16 +0200

    Add files via upload

---
+++ src/tempDB.js
@@ -0,0 +1,45 @@
+export const tempDB = [
+	{
+		id: 1,
+		name: "Dragos Tabacu",
+		address: "Str. Florilor 5/4, Panciu, Bacău, CP 973113",
+		picture: null,
+	},
+	{
+		id: 2,
+		name: "Homner Dinu",
+		address:
+			"P-ța Traian nr. 815, bl. 10, et. 0, ap. 25, Mun. Caracal, Timiș, CP 697719",
+		picture: null,
+	},
+	{
+		id: 3,
+		name: "Ania Lazăr",
+		address: "Aleea Învățătorului 1A, Baia de Aramă, Harghita, CP 723824",
+		picture: null,
+	},
+	{
+		id: 21,
+		name: "Emilian Rusu",
+		address: "Mun. Azuga, Sibiu, CP 960794",
+		picture: null,
+	},
+	{
+		id: 31,
+		name: "Emilian Ioniță",
+		address: "Str. Cireșilor 3A, Mun. Jibou, Tulcea, CP 975862",
+		picture: null,
+	},
+	{
+		id: 41,
+		name: "Laura Radulescu",
+		address: "B-dul. Eroilor 8A, Câmpeni, Ilfov, CP 514011",
+		picture: null,
+	},
+	{
+		id: 51,
+		name: "Tania Mhădă",
+		address: "B-dul. Frasinului 122, Mun. Techirghiol, Galați, CP 003956",
+		picture: null,
+	},
+];
